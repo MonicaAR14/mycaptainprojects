f6301a273f27b003d5d170da5bb37451e80d6dd6
MonicaAR14 <58458478+MonicaAR14@users.noreply.github.com>
2021-07-03 12:51:47 +0530

    Add files via upload

---
+++ cinout.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int age;
+    cout<<"Enter age";
+    cin>>age;
+    cout<<"Your age"<<age;
+}
---
+++ task2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b, sum;
+    cout<<"Enter two numbers"<<endl;
+    cin>>a>>b;
+    sum = a* b;
+    cout<<"Product = "<<sum;
+    return 0;
+}
